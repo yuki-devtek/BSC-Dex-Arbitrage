b8612276d6b6f16a49cc2a884d3df8853189a16d
yuki-devtek <yukikoga59@gmail.com>
2021-12-29 12:59:53 -0500

    Money's only something you need in case you don't die tomorrow

--- deploy/00_Arbitrage.js
+++ deploy/00_Arbitrage.js
@@ -1,8 +1,19 @@
-module.exports = async ({ getNamedAccounts, deployments}) => {
+module.exports = async ({ getNamedAccounts, deployments, getChainId}) => {
   const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
+    const chainId  = await getChainId();
+    
     // ChiToken only seems to work correctly on mainnet, due to assembly usage
-    const bsc_mainnet_chiToken = "0x0000000000004946c0e9F43F4Dee607b0eF1fA1c";
+    let chiToken;
+    if (chainId == 56) { 
+      chiToken = "0x0000000000004946c0e9F43F4Dee607b0eF1fA1c";
+    } else if (chainId == 97) {
+      chiToken = "0x83a23e36ca483779704378aa0573c4e0f37d729e";
+    } else {
+      console.log("Unkown ChainId - try again");
+      process.exit();
+    }
+    
     // array of whitelisted wallets who also can swap
     // Contract deployer wallet is automatically whitelisted on contract creation
     // Notice:  All swapped funds are sent to contract and onlyOwner can withdraw
@@ -10,7 +21,7 @@ module.exports = async ({ getNamedAccounts, deployments}) => {
     await deploy("Arbitrage", {
       from: deployer,
       log: true,
-      args: [bsc_mainnet_chiToken, arbWallets]
+      args: [chiToken, arbWallets]
     });
   };
   module.exports.tags = ["Arbitrage"];
