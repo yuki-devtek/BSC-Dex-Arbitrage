cf5c38d3da3c70765d376e51372ba151891ae3a9
yuki-devtek <yukikoga59@gmail.com>
2022-04-27 02:08:07 -0400

    If you build it, he will come...

--- app/getAmoutsOut.js
+++ app/getAmoutsOut.js
@@ -11,21 +11,25 @@ const arbContract = new ethers.Contract(process.env.CONTRACT_ADDRESS, arbABI, ac
 
 const get = async () => {
     try {
-        let fees = [];
-        fees.push(process.env.FEES);
-        let tx = await arbContract.hawk(
-            ethers.utils.parseUnits(process.env.PURCHASE_AMOUNT, "ether"),
-            process.env.TOKEN_BASE,
-            process.env.TOKEN_TARGET,
-            process.env.ROUTER,
-            fees,
-            Date.now() + 1000 * process.env.DEADLINE,
-            {
-                gasLimit: process.env.GAS_LIMIT,
-                gasPrice: ethers.utils.parseUnits(process.env.GAS_PRICE, "gwei"),
-            }
-        );
-        console.log("TxHash: " + tx.hash);
+        const path = [
+            "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+            "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+            "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+          ];
+          const pairPath = [
+            "0x74E4716E431f45807DCF19f284c7aA99F18a4fbc",
+            "0xA0C3Ef24414ED9C9B456740128d8E63D016A9e11"
+          ];
+        const fees = [25, 25];
+        const amountIn = "1000000000000000000";
+        const amounts = await arbContract.getAmountsOut(amountIn, path, pairPath, fees);
+        console.log(amounts);
+        const a0 = ethers.utils.formatUnits(amounts[0], "ether");
+        const a1 = ethers.utils.formatUnits(amounts[1], "ether");
+        const a2 = ethers.utils.formatUnits(amounts[2], "ether");
+        console.log(a0);
+        console.log(a1);
+        console.log(a2);
         console.log("-FIN-");
         process.exit();
     } catch (e) {
